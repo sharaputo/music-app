181581ecc175ddeb3a4b05381525a5321c13fab7
Fedor Sharaputo <sharaputo@icloud.com>
2022-12-24 06:39:51 +0300

    fix for one word components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,12 @@ module.exports = {
       files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
       extends: ["plugin:cypress/recommended"],
     },
+    {
+      files: ["src/views/**/*.vue"],
+      rules: {
+        "vue/multi-word-component-names": 0,
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
